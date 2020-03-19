f66613eecbae3faa079b87917faf2897db158852
sunfujiancn <41288938+sunfujiancn@users.noreply.github.com>
2020-03-19 23:01:06 +0800

    init project

---
+++ src/main/java/cn/sunfj/bookmarksweb/BookmarksWebApplication.java
@@ -0,0 +1,13 @@
+package cn.sunfj.bookmarksweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarksWebApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(BookmarksWebApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/cn/sunfj/bookmarksweb/BookmarksWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sunfj.bookmarksweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarksWebApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
